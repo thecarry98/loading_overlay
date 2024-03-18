819234f20ffc31ba4508d653a69322a523470238
Duong Viet Anh <anhdv@vivas.vn>
2024-03-18 16:28:57 +0700

    init

---
+++ example/lib/main.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:loader_overlay/loader_overlay.dart';
+
+void main() => runApp(MyAppGlobalLoaderOverlay());
+
+class MyAppGlobalLoaderOverlay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GlobalLoaderOverlay(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primaryColor: Colors.black,
+          fontFamily: 'Baloo',
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => MyHomePage(),
+        },
+      ),
+      overlayColor: Colors.grey.withOpacity(0.8),
+      useDefaultLoading: false,
+      overlayWidgetBuilder: (_) { //ignored progress for the moment
+        return Center(
+          child: SpinKitCubeGrid(
+            color: Colors.red,
+            size: 50.0,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _isLoaderVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () async {
+                context.loaderOverlay.show();
+                setState(() {
+                  _isLoaderVisible = context.loaderOverlay.visible;
+                });
+                await Future.delayed(Duration(seconds: 2));
+                if (_isLoaderVisible) {
+                  context.loaderOverlay.hide();
+                }
+                setState(() {
+                  _isLoaderVisible = context.loaderOverlay.visible;
+                });
+              },
+              child: Text('Show overlay for 2 seconds'),
+            ),
+            SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () async {
+                context.loaderOverlay.show(
+                    widgetBuilder: (progress) {
+                      return ReconnectingOverlay(
+                        progress != null ? progress as String : null,
+                      );
+                    },
+                    progress: 'Trying to reconnect');
+                setState(() {
+                  _isLoaderVisible = context.loaderOverlay.visible;
+                });
+                await Future.delayed(Duration(seconds: 3));
+                if (_isLoaderVisible) {
+                  context.loaderOverlay.hide();
+                }
+                setState(() {
+                  _isLoaderVisible = context.loaderOverlay.visible;
+                });
+              },
+              child: Text('Show custom loader overlay for 2 seconds'),
+            ),
+            SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () async {
+                context.loaderOverlay.show(
+                  progress: 'Doing progress #0',
+                );
+                setState(() {
+                  _isLoaderVisible = context.loaderOverlay.visible;
+                });
+
+                await Future.delayed(Duration(seconds: 1));
+                context.loaderOverlay.progress('Doing progress #1');
+                await Future.delayed(Duration(seconds: 1));
+                context.loaderOverlay.progress('Doing progress #2');
+                await Future.delayed(Duration(seconds: 1));
+                context.loaderOverlay.progress('Doing progress #3');
+                await Future.delayed(Duration(seconds: 1));
+
+                if (_isLoaderVisible) {
+                  context.loaderOverlay.hide();
+                }
+                setState(() {
+                  _isLoaderVisible = context.loaderOverlay.visible;
+                });
+              },
+              child: Text('Show loader overlay for 5 seconds with progress'),
+            ),
+            SizedBox(height: 34),
+            Text('Is loader visible: $_isLoaderVisible'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ReconnectingOverlay extends StatelessWidget {
+  final String? progress;
+
+  ReconnectingOverlay(this.progress);
+
+  @override
+  Widget build(BuildContext context) => Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(),
+            SizedBox(height: 12),
+            Text(
+              'Reconnecting...',
+            ),
+            SizedBox(height: 12),
+            Text(
+              progress ?? '',
+            ),
+          ],
+        ),
+      );
+}
---
+++ test/loading_overlay_test.dart
@@ -0,0 +1,317 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:loader_overlay/loader_overlay.dart';
+
+void main() {
+  testWidgets('default properties', (WidgetTester tester) async {
+    await tester.pumpWidget(const TestApp());
+
+    expect(
+      find.byKey(LoaderOverlay.defaultOverlayWidgetKey, skipOffstage: false),
+      findsNothing,
+    );
+
+    // click to show overlay widget
+
+    await tester.tap(
+      find.byKey(TestApp.showHideOverlayIconKey),
+    );
+
+    await tester.pump();
+
+    expect(
+      find.byKey(LoaderOverlay.defaultOverlayWidgetKey),
+      findsOneWidget,
+    );
+
+    final opacityFinder = find.byKey(LoaderOverlay.containerForOverlayColorKey);
+
+    expect(opacityFinder, findsOneWidget);
+
+    final containerFinder =
+        find.byKey(LoaderOverlay.containerForOverlayColorKey);
+
+    expect(containerFinder, findsOneWidget);
+
+    final containerWidget = tester.firstWidget(containerFinder) as ColoredBox;
+
+    expect(
+      containerWidget.color,
+      equals(LoaderOverlay.defaultOverlayColor),
+    );
+
+    await tester.pump(Duration.zero);
+
+    expect(
+      find.byKey(LoaderOverlay.defaultOverlayWidgetKey, skipOffstage: false),
+      findsNothing,
+    );
+
+    expect(
+      find.byKey(LoaderOverlay.containerForOverlayColorKey,
+          skipOffstage: false),
+      findsNothing,
+    );
+  });
+
+  testWidgets(
+      'custom overlayWidget is ignored when `useDefaultLoading` is not set to `false`',
+      (WidgetTester tester) async {
+    const containerKey = Key('b');
+
+    await tester.pumpWidget(
+      TestApp(
+        // useDefaultLoading is true by default
+        overlayWidgetBuilder: (progress) => Container(key: containerKey),
+      ),
+    );
+
+    // default overlay widget is not visible
+
+    expect(
+      find.byKey(LoaderOverlay.defaultOverlayWidgetKey, skipOffstage: false),
+      findsNothing,
+    );
+
+    // custom overlay widget is not visible
+
+    expect(
+      find.byKey(containerKey, skipOffstage: false),
+      findsNothing,
+    );
+
+    // click to show overlay widget
+
+    await tester.tap(
+      find.byKey(TestApp.showHideOverlayIconKey),
+    );
+
+    await tester.pump();
+
+    // custom overlay widget is never shown (ignored) because
+    // `useDefaultLoading` has not been set to `false`
+
+    expect(
+      find.byKey(containerKey, skipOffstage: false),
+      findsNothing,
+    );
+
+    // we will still show the default overlay widget even though user specified
+    // a custom loader for the reason given above
+
+    expect(
+      find.byKey(LoaderOverlay.defaultOverlayWidgetKey),
+      findsOneWidget,
+    );
+
+    await tester.pump(Duration.zero);
+
+    expect(
+      find.byKey(LoaderOverlay.defaultOverlayWidgetKey, skipOffstage: false),
+      findsNothing,
+    );
+  });
+  testWidgets(
+      'custom overlayWidget is used when `useDefaultLoading` is set to `false`',
+      (WidgetTester tester) async {
+    const containerKey = Key('b');
+
+    await tester.pumpWidget(
+      TestApp(
+        overlayWidgetBuilder: (progress) => Container(key: containerKey),
+        useDefaultLoading: false,
+      ),
+    );
+
+    expect(
+      find.byKey(containerKey, skipOffstage: false),
+      findsNothing,
+    );
+
+    await tester.tap(
+      find.byKey(TestApp.showHideOverlayIconKey),
+    );
+
+    await tester.pump();
+
+    // we will show user's custom overlay widget
+
+    expect(
+      find.byKey(containerKey),
+      findsOneWidget,
+    );
+
+    // we will not show the default overlay widget because user specified a
+    // custom overlay widget and set `useDefaultLoading` to false
+
+    expect(
+      find.byKey(LoaderOverlay.defaultOverlayWidgetKey, skipOffstage: false),
+      findsNothing,
+    );
+
+    await tester.pump(Duration.zero);
+
+    expect(
+      find.byKey(containerKey, skipOffstage: false),
+      findsNothing,
+    );
+  });
+
+  testWidgets('custom `overlayColor` and `overlayOpacity`',
+      (WidgetTester tester) async {
+    const overlayColor = Colors.red;
+    const overlayOpacity = 0.8;
+
+    await tester.pumpWidget(
+      const TestApp(overlayColor: overlayColor),
+    );
+
+    expect(
+      find.byKey(LoaderOverlay.defaultOverlayWidgetKey, skipOffstage: false),
+      findsNothing,
+    );
+
+    await tester.tap(
+      find.byKey(TestApp.showHideOverlayIconKey),
+    );
+
+    await tester.pump();
+
+    expect(
+      find.byKey(LoaderOverlay.defaultOverlayWidgetKey),
+      findsOneWidget,
+    );
+
+    expect(
+      overlayOpacity,
+      equals(overlayOpacity),
+    );
+
+    final containerFinder =
+        find.byKey(LoaderOverlay.containerForOverlayColorKey);
+
+    expect(containerFinder, findsOneWidget);
+
+    final containerWidget = tester.firstWidget(containerFinder) as ColoredBox;
+
+    expect(
+      containerWidget.color,
+      isNot(
+        equals(LoaderOverlay.defaultOverlayColor),
+      ),
+    );
+
+    expect(
+      containerWidget.color,
+      equals(overlayColor),
+    );
+
+    await tester.pump(Duration.zero);
+
+    expect(
+      find.byKey(LoaderOverlay.defaultOverlayWidgetKey, skipOffstage: false),
+      findsNothing,
+    );
+  });
+
+  testWidgets('loading widget without no overlay layer',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(const TestApp());
+
+    expect(
+      find.byKey(LoaderOverlay.defaultOverlayWidgetKey, skipOffstage: false),
+      findsNothing,
+    );
+
+    // click to show overlay widget
+
+    await tester.tap(
+      find.byKey(TestApp.showHideOverlayOptionalKey),
+    );
+
+    await tester.pump();
+
+    expect(
+      find.byKey(LoaderOverlay.defaultOverlayWidgetKey),
+      findsOneWidget,
+    );
+
+    final opacityFinder = find.byKey(LoaderOverlay.containerForOverlayColorKey);
+
+    expect(opacityFinder, findsNothing);
+
+    final containerFinder =
+        find.byKey(LoaderOverlay.containerForOverlayColorKey);
+
+    expect(containerFinder, findsNothing);
+
+    await tester.pump(Duration.zero);
+
+    expect(
+      find.byKey(LoaderOverlay.defaultOverlayWidgetKey, skipOffstage: false),
+      findsNothing,
+    );
+
+    expect(
+      find.byKey(LoaderOverlay.containerForOverlayColorKey,
+          skipOffstage: false),
+      findsNothing,
+    );
+  });
+}
+
+class TestApp extends StatelessWidget {
+  const TestApp({
+    Key? key,
+    this.overlayWidgetBuilder,
+    this.useDefaultLoading = LoaderOverlay.useDefaultLoadingValue,
+    this.overlayColor,
+  }) : super(key: key);
+
+  final Widget Function(dynamic progress)? overlayWidgetBuilder;
+  final bool useDefaultLoading;
+  final Color? overlayColor;
+
+  static const showHideOverlayIconKey = Key('@test/show-hide-overlay');
+  static const showHideOverlayOptionalKey =
+      Key('@test/show-hide-overlay-optional');
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoaderOverlay(
+        overlayWidgetBuilder: overlayWidgetBuilder,
+        useDefaultLoading: useDefaultLoading,
+        overlayColor: overlayColor ?? LoaderOverlay.defaultOverlayColor,
+        child: Scaffold(
+          body: Column(
+            children: [
+              IconButton(
+                key: TestApp.showHideOverlayIconKey,
+                onPressed: () async {
+                  context.loaderOverlay.show();
+
+                  await Future.delayed(Duration.zero);
+
+                  context.loaderOverlay.hide();
+                },
+                icon: const Icon(Icons.add),
+              ),
+              IconButton(
+                key: TestApp.showHideOverlayOptionalKey,
+                onPressed: () async {
+                  context.loaderOverlay.show(showOverlay: false);
+
+                  await Future.delayed(Duration.zero);
+
+                  context.loaderOverlay.hide();
+                },
+                icon: const Icon(Icons.add),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
